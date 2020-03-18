8ede08c190f0f1d536fb8fbb5028dce23f77a740
Kenny  Frueh <kenfrueh@uat.edu>
2020-03-18 12:16:15 -0700

    Added Walking animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Transform tf;
     private Rigidbody2D rb2d;
     private SpriteRenderer sr;
+    private Animator ani;
 
     public float speed = 5.0f;
     public float jumpForce = 200.0f;
@@ -19,14 +20,28 @@ public class PlayerController : MonoBehaviour
         tf = gameObject.GetComponent<Transform>();
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         sr = gameObject.GetComponent<SpriteRenderer>();
+        ani = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {//Moving on the screen
         float xMovement = Input.GetAxis("Horizontal") * speed;
         rb2d.velocity = new Vector2(xMovement, rb2d.velocity.y);
-        sr.flipX = rb2d.velocity.x < 0;
+        if (xMovement > 0)
+        {
+            sr.flipX = false;
+            ani.Play("PlayerWalk");
+        }
+        else if (xMovement < 0)
+        {
+            sr.flipX = true;
+            ani.Play("PlayerWalk");
+        }
+        else
+        {
+            ani.Play("PlayerIdle");
+        }
         //Detect if player is on ground
         RaycastHit2D hitInfo = Physics2D.Raycast(groundPoint.position, Vector2.down, 1f);
         if (hitInfo.collider != null)
